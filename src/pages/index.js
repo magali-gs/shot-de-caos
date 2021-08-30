742d3a47e6dbc98d0ba03eaf009bc8e98af41e17
Magali Goncalves <magali.goncalvess@gmail.com>
2021-08-30 19:08:55 +0200

    adding all the blog code gatsby setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,60 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import { Link, graphql } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
 
-export default IndexPage
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`
+
+export default ({ data }) => {
+  console.log(data)
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <div>
+        <h1>Shot de Caos</h1>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <BlogLink to={node.fields.slug}>
+              <span>
+                {node.frontmatter.title} -{node.frontmatter.date}
+              </span>
+            </BlogLink>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
+        <h4>{data.allMarkdownRemark.totalCount}</h4>
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            date
+            description
+            title
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
